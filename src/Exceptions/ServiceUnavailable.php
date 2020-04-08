cbc6a01547cd1f1f1a0de10bcdfc201a25b213d1
Abdullah Al-Faqeir <abdullah@devloops.net>
2020-04-08 20:18:46 +0300

    - Failure retries + recovery. - Add client wrapper for import of documents. - Support peering configuration.

---
+++ src/Exceptions/ServiceUnavailable.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Devloops\Typesence\Exceptions;
+
+/**
+ * Class ServiceUnavailable
+ *
+ * @package Devloops\Typesence\Exceptions
+ * @date    4/8/20
+ * @author  Abdullah Al-Faqeir <abdullah@devloops.net>
+ */
+class ServiceUnavailable extends TypesenseClientError
+{
+
+}
