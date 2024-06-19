f2d043e1b455fbb1965e89245118e5d9d838711c
Hayden <hayden.hhoang@gmail.com>
2024-06-19 11:44:16 +0700

    test: key create

--- tests/Feature/KeyTest.php
+++ tests/Feature/KeyTest.php
@@ -9,18 +9,25 @@ use Typesense\Exceptions\ObjectNotFound;
 class KeyTest extends TestCase
 {
     private $keyId = null;
+    private $createKeyResponse = null;
+
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $response =  $this->client()->keys->create([
+        $res =  $this->client()->keys->create([
             'description' => 'Admin key.',
             'actions' => ['*'],
             'collections' => ['*']
-        ]);;
-        $this->keyId = $response['id'];
-        $this->assertEquals($response['description'], 'Admin key.');
+        ]);
+        $this->createKeyResponse = $res;
+        $this->keyId = $res['id'];
+    }
+
+    public function testCanCreateAKey(): void
+    {
+        $this->assertEquals($this->createKeyResponse['description'], 'Admin key.');
     }
 
     public function testCanRetrieveAKey(): void
