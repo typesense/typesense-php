f43e14c4da786fad521ece3807ab947193ffb80a
Hayden <hayden.hhoang@gmail.com>
2024-06-20 08:25:36 +0700

    test: analytics events

---
+++ tests/Feature/AnalyticsEventsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Feature;
+
+use Tests\TestCase;
+
+class AnalyticsEventsTest extends TestCase
+{
+    //* there is no method for sending events
+    // public function testCanCreateAnEvent(): void
+    // {
+    //     $returnData = $this->client()->analytics->rules()->even
+    //     $this->assertEquals($returnData['name'], $this->ruleName);
+    // }
+
+    public function testNeedImplementationForAnalyticsEvents(): void
+    {
+        $this->assertTrue(true);
+    }
+}
