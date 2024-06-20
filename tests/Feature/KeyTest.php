7d0814535315df1e8535e657c9e101158abb4893
Hayden <hayden.hhoang@gmail.com>
2024-06-20 16:42:33 +0700

    test: retrieve all keys & generateScopedSearchKey

--- tests/Feature/KeyTest.php
+++ tests/Feature/KeyTest.php
@@ -25,6 +25,14 @@ class KeyTest extends TestCase
         $this->keyId = $res['id'];
     }
 
+    protected function tearDown(): void
+    {
+        $keys = $this->client()->keys->retrieve();
+        foreach ($keys['keys'] as $key) {
+            $this->client()->keys[$key['id']]->delete();
+        }
+    }
+
     public function testCanCreateAKey(): void
     {
         $this->assertEquals($this->createKeyResponse['description'], 'Admin key.');
@@ -44,5 +52,25 @@ class KeyTest extends TestCase
 
         $this->expectException(ObjectNotFound::class);
         $key = $this->client()->keys[$this->keyId]->retrieve();
+    }
+
+    public function testCanRetrieveAllKeys(): void
+    {
+        $keys = $this->client()->keys->retrieve();
+        $this->assertCount(1, $keys['keys']);
+    }
+
+    public function testCanGenerateScopedSearchKey(): void
+    {
+        // The following keys were generated and verified to work with an actual Typesense server
+        // We're only verifying that the algorithm works as expected client-side
+        $searchKey = "RN23GFr1s6jQ9kgSNg2O7fYcAUXU7127";
+        $scopedSearchKey =
+            "SC9sT0hncHFwTHNFc3U3d3psRDZBUGNXQUViQUdDNmRHSmJFQnNnczJ4VT1STjIzeyJmaWx0ZXJfYnkiOiJjb21wYW55X2lkOjEyNCJ9";
+
+        $result = $this->client()->keys->generateScopedSearchKey($searchKey, [
+            "filter_by" => "company_id:124",
+        ]);
+        $this->assertEquals($scopedSearchKey, $result);
     }
 }
