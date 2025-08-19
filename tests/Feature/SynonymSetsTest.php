9ebff7d84357a0ada27a7dc6dffd941db27c1546
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-08-19 18:25:09 +0300

    test: add test suite for synonym sets

---
+++ tests/Feature/SynonymSetsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Feature;
+
+use Tests\TestCase;
+use Typesense\Exceptions\ObjectNotFound;
+use Exception;
+
+class SynonymSetsTest extends TestCase
+{
+    private $upsertResponse = null;
+    private $synonymSets = null;
+    private $synonymSetData = [
+        'synonyms' => [
+            [
+                'id' => 'dummy',
+                'synonyms' => ['foo', 'bar', 'baz'],
+                'root' => '',
+            ],
+        ],
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        if (!$this->isV30OrAbove()) {
+            $this->markTestSkipped('SynonymSets is only supported in Typesense v30+');
+        }
+        
+        $this->synonymSets = $this->client()->synonymSets;
+        $this->upsertResponse = $this->synonymSets->upsert('test-synonym-set', $this->synonymSetData);
+    }
+
+
+    public function testCanUpsertASynonymSet(): void
+    {
+        $this->assertEquals($this->synonymSetData['synonyms'], $this->upsertResponse['synonyms']);
+    }
+
+    public function testCanRetrieveAllSynonymSets(): void
+    {
+        $returnData = $this->synonymSets->retrieve();
+        $this->assertCount(1, $returnData);
+    }
+
+    public function testCanRetrieveASpecificSynonymSet(): void
+    {
+        $returnData = $this->synonymSets['test-synonym-set']->retrieve();
+        $this->assertEquals($this->synonymSetData['synonyms'], $returnData['synonyms']);
+    }
+
+    public function testCanDeleteASynonymSet(): void
+    {
+        $returnData = $this->synonymSets['test-synonym-set']->delete();
+        $this->assertEquals('test-synonym-set', $returnData['name']);
+
+        $this->expectException(ObjectNotFound::class);
+        $this->synonymSets['test-synonym-set']->retrieve();
+    }
+} 
